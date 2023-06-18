1a78fb3da2e648f463338319be31e35f2179294e
Jonh rdz <j.rodriguez2018ng@clases.edu.sv>
2023-06-18 14:28:10 -0600

    Migrations

--- Infraestructure/Persistence/Configuration/ConfigurationCustomer.cs
+++ Infraestructure/Persistence/Configuration/ConfigurationCustomer.cs
@@ -9,7 +9,7 @@ public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
 {
     public void Configure(EntityTypeBuilder<Customer> builder)
     {
-        // builder.ToTable("Customers");
+        builder.ToTable("Customers");
 
         builder.HasKey(c => c.Id);
 
